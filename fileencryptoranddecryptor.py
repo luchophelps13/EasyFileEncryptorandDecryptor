e4362203277da9937b030b3477bc76bc2d0f04f5
luchophelps13 <53153598+luchophelps13@users.noreply.github.com>
2020-11-09 14:37:48 -0600

    Add files via upload

---
+++ fileencryptoranddecryptor.py
@@ -0,0 +1,66 @@
+from cryptography.fernet import Fernet
+import os 
+
+class EncryptYourFiles:
+
+    def __init__(self, PATH):
+
+        self.PATH = PATH
+
+    def make_key(self):
+
+        k = Fernet.generate_key()
+
+        global key
+        key = Fernet(k)
+
+    def go_to_directory(self):
+
+        os.chdir(PATH)
+
+        print(os.listdir())
+
+    def encrypt(self):
+
+        for filename in os.listdir(os.getcwd()):
+            
+            with open(os.path.join(os.getcwd(), filename), 'r') as f: 
+
+                data = f.read().encode()
+
+            with open(os.path.join(os.getcwd(), filename), 'wb') as same_file: 
+
+                encrypted = key.encrypt(data)
+
+                same_file.truncate(0)
+
+                same_file.write(encrypted)
+
+                print(encrypted.decode())
+
+    def decrypt(self):
+
+        for filename in os.listdir(os.getcwd()):
+
+            with open(os.path.join(os.getcwd(), filename), 'rb') as f: 
+
+                contents = f.read()
+    
+            with open(os.path.join(os.getcwd(), filename), 'wb') as f: 
+                
+                decrypted = key.decrypt(contents)
+
+                decrypted.decode()
+
+                f.write(decrypted)
+
+                print(decrypted.decode())
+
+PATH = "C:\\Users\\luchc\\Python Projects\\Cryptography\\Encrypted Files"
+
+encryptor = EncryptYourFiles(PATH)
+
+encryptor.make_key()
+encryptor.go_to_directory()
+encryptor.encrypt()
+encryptor.decrypt()
